5124d757ed0a5b788ef671e5402589ef3af44738
csrl1815 <50893788+csrl1815@users.noreply.github.com>
2023-12-06 17:27:51 -0600

    commit 2

---
+++ Almacen.Persistence/Migrations/20231206221559_ApplicationDdContext.cs
@@ -0,0 +1,245 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Almacen.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplicationDdContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id_Categoria = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre_Categoria = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id_Categoria);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Marcas",
+                columns: table => new
+                {
+                    Id_Marca = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre_Marca = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marcas", x => x.Id_Marca);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Permissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Controller = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Action = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permissions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Proveedores",
+                columns: table => new
+                {
+                    Id_Proveedor = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre_Proveedor = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proveedores", x => x.Id_Proveedor);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sucursales",
+                columns: table => new
+                {
+                    Id_Sucursal = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre_Sucursal = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sucursales", x => x.Id_Sucursal);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Email);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    Id_Producto = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre_Producto = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Precio_Compra = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Precio_Venta = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Id_Categoria = table.Column<int>(type: "int", nullable: false),
+                    Id_Proveedor = table.Column<int>(type: "int", nullable: false),
+                    Id_Marca = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.Id_Producto);
+                    table.ForeignKey(
+                        name: "FK_Productos_Categorias_Id_Categoria",
+                        column: x => x.Id_Categoria,
+                        principalTable: "Categorias",
+                        principalColumn: "Id_Categoria",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Productos_Marcas_Id_Marca",
+                        column: x => x.Id_Marca,
+                        principalTable: "Marcas",
+                        principalColumn: "Id_Marca",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Productos_Proveedores_Id_Proveedor",
+                        column: x => x.Id_Proveedor,
+                        principalTable: "Proveedores",
+                        principalColumn: "Id_Proveedor",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PolicyPermissionSystemUser",
+                columns: table => new
+                {
+                    PermissionsId = table.Column<int>(type: "int", nullable: false),
+                    UsersEmail = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PolicyPermissionSystemUser", x => new { x.PermissionsId, x.UsersEmail });
+                    table.ForeignKey(
+                        name: "FK_PolicyPermissionSystemUser_Permissions_PermissionsId",
+                        column: x => x.PermissionsId,
+                        principalTable: "Permissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PolicyPermissionSystemUser_Users_UsersEmail",
+                        column: x => x.UsersEmail,
+                        principalTable: "Users",
+                        principalColumn: "Email",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos_Sucursales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    Id_Sucursal = table.Column<int>(type: "int", nullable: false),
+                    Id_Producto = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos_Sucursales", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Productos_Sucursales_Productos_Id_Producto",
+                        column: x => x.Id_Producto,
+                        principalTable: "Productos",
+                        principalColumn: "Id_Producto",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Productos_Sucursales_Sucursales_Id_Sucursal",
+                        column: x => x.Id_Sucursal,
+                        principalTable: "Sucursales",
+                        principalColumn: "Id_Sucursal",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PolicyPermissionSystemUser_UsersEmail",
+                table: "PolicyPermissionSystemUser",
+                column: "UsersEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productos_Id_Categoria",
+                table: "Productos",
+                column: "Id_Categoria");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productos_Id_Marca",
+                table: "Productos",
+                column: "Id_Marca");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productos_Id_Proveedor",
+                table: "Productos",
+                column: "Id_Proveedor");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productos_Sucursales_Id_Producto",
+                table: "Productos_Sucursales",
+                column: "Id_Producto");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productos_Sucursales_Id_Sucursal",
+                table: "Productos_Sucursales",
+                column: "Id_Sucursal");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PolicyPermissionSystemUser");
+
+            migrationBuilder.DropTable(
+                name: "Productos_Sucursales");
+
+            migrationBuilder.DropTable(
+                name: "Permissions");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "Sucursales");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+
+            migrationBuilder.DropTable(
+                name: "Marcas");
+
+            migrationBuilder.DropTable(
+                name: "Proveedores");
+        }
+    }
+}
